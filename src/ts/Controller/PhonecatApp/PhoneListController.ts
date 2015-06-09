f22c93d90cff06d80da829f95b310f2dcc49ad8f
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2015-06-09 22:27:00 +0900

    add orderProp to index.html and Controller.ts

--- src/ts/Controller/PhonecatApp/PhoneListController.ts
+++ src/ts/Controller/PhonecatApp/PhoneListController.ts
@@ -7,23 +7,28 @@ interface Phone {
 
 module Controller.PhonecatApp {
     export class PhoneListController {
-        public phones:Phone[];
+        public phones:    Phone[];
+        public orderProp: string;
 
         constructor () {
             this.phones = [
                 {
                     'name': 'Nexus S',
-                    'snippet': 'Fast just got faster with Nexus S.'
+                    'snippet': 'Fast just got faster with Nexus S.',
+                    'age': 1
                 },
                 {
                     'name': 'Motorola XOOM™ with Wi-Fi',
-                    'snippet': 'The Next, Next Generation tablet.'
+                    'snippet': 'The Next, Next Generation tablet.',
+                    'age': 2
                 },
                 {
                     'name': 'MOTOROLA XOOM™',
-                    'snippet': 'The Next, Next Generation tablet.'
+                    'snippet': 'The Next, Next Generation tablet.',
+                    'age': 3
                 }
             ];
+            this.orderProp = 'age';
         }
 
         static init () {
