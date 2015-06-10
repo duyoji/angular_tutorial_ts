49e573976b865ad62d9b23291c34e73b092a5224
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2015-06-10 22:58:03 +0900

    add unit test for xhr

--- test/unit/ts/Controller/PhonecatApp/PhoneListControllerSpec.ts
+++ test/unit/ts/Controller/PhonecatApp/PhoneListControllerSpec.ts
@@ -6,15 +6,32 @@ describe('PhoneCat controllers', function() {
 
     describe('PhoneListController', function(){
         var ctrl: any;
+        var $httpBackend: angular.IHttpBackendService;
 
         beforeEach(module('PhonecatApp'));
 
-        beforeEach(inject(function($controller:any) {
-            ctrl = $controller('PhoneListController');
-        }));
+        beforeEach(
+            inject(
+                function(
+                    $controller: any,
+                    _$httpBackend_: angular.IHttpBackendService
+                ) {
+                    $httpBackend = _$httpBackend_;
+                    $httpBackend.expectGET('data/phones/phones.json')
+                        .respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+                    ctrl = $controller('PhoneListController');
+                }
+                )
+            );
 
-        it('should create "phones" model with 3 phones', function() {
-            expect(ctrl.phones.length).toBe(3);
+        it('should create "phones" model with 2 phones fetched from xhr', function() {
+            expect(ctrl.phones).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(ctrl.phones).toEqual([
+                {name: 'Nexus S'},
+                {name: 'Motorola DROID'}
+            ]);
         });
 
         it('should set the default value of orderProp model', function() {
