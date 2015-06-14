be22839035008c731b100405b5053704e4a6da01
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2015-06-14 11:47:24 +0900

    create unit test for PhoneDetailContoller that is empty

---
+++ test/unit/ts/Controller/PhonecatApp/PhoneDetailControllerSpec.ts
@@ -0,0 +1,10 @@
+/// <reference path="../../../../../typings/angularjs/angular-mocks.d.ts" />
+/// <reference path="../../../../../typings/jasmine/jasmine.d.ts" />
+
+/* jasmine specs for controllers go here */
+describe('PhoneCat controllers', function() {
+
+    describe('PhoneDetailController', function(){
+    });
+});
+
