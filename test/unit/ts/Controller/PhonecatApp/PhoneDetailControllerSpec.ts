30b17793e61541cd748496b5134c7e3b5ee9f28c
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2015-06-14 16:56:52 +0900

    add unit test to PhoneDetailController that access with phoneId

--- test/unit/ts/Controller/PhonecatApp/PhoneDetailControllerSpec.ts
+++ test/unit/ts/Controller/PhonecatApp/PhoneDetailControllerSpec.ts
@@ -3,8 +3,36 @@
 
 /* jasmine specs for controllers go here */
 describe('PhoneCat controllers', function() {
-
     describe('PhoneDetailController', function(){
+        var ctrl: any;
+        var $httpBackend: angular.IHttpBackendService;
+
+        beforeEach(module('PhonecatApp'));
+
+        beforeEach(
+            inject(
+                function(
+                    $controller: any,
+                    _$httpBackend_: angular.IHttpBackendService,
+                    $routeParams: any
+                ) {
+                    $httpBackend = _$httpBackend_;
+                    $httpBackend
+                        .expectGET('data/phones/xyz.json')
+                        .respond({name:'phone xyz'});
+                    $routeParams.phoneId = 'xyz';
+                    ctrl = $controller('PhoneDetailController');
+                }
+            )
+        );
+
+
+        it('should fetch phone detail', function() {
+            expect(ctrl.phone).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(ctrl.phone).toEqual({name:'phone xyz'});
+        });
     });
 });
 
