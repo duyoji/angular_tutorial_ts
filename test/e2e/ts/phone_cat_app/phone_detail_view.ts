e15592433d5f1d173f41682d165210fef5451beb
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2015-06-14 17:55:56 +0900

    add e2d test for thumbnails src verify

--- test/e2e/ts/phone_cat_app/phone_detail_view.ts
+++ test/e2e/ts/phone_cat_app/phone_detail_view.ts
@@ -8,5 +8,29 @@ describe('Phone detail view', function() {
 
     it('should display nexus-s page', function() {
         expect(element(by.binding('c.phone.name')).getText()).toBe('Nexus S');
+
+        // thumbnails
+        var thumbnailUrls =
+            element
+                .all(by.repeater('img in c.phone.images'))
+                .all(by.tagName('img'));
+
+        function getNgSrcValues() {
+            return thumbnailUrls.map(function(elm) {
+                return elm.getAttribute('ng-src');
+            });
+        }
+
+        expect(thumbnailUrls.count()).toBe(4);
+        expect(getNgSrcValues()).toEqual(
+            [
+                "img/phones/nexus-s.0.jpg",
+                "img/phones/nexus-s.1.jpg",
+                "img/phones/nexus-s.2.jpg",
+                "img/phones/nexus-s.3.jpg"
+            ]
+        );
+
+
     });
 });
